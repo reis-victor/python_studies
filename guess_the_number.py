d69ada3b1bd377816dae2cd5304b60305c222666
Victor Reis <victorreis777@gmail.com>
2019-11-18 23:52:27 -0300

    Updated code

--- guess_the_number.py
+++ guess_the_number.py
@@ -6,27 +6,33 @@ number_generator = secrets.SystemRandom()
 
 number = number_generator.randint(1, 10)
 
-# The following code is used to print the generated number: print("The random number is", number)
+print("The random number is", number)
 
-print("Please, guess a number between 1 and 10")
+print('Please, guess a number between 1 and 10')
 
 # Asks the user for input and checks if it is an integer number
 while True:
-    user_number = input("Your number is: ")
     try:
-      user_number = int(user_number)
-      break
+        user_number = int(input('Your number is: '))
+        if 10 >= user_number >= 1:
+            break
+        print('Please, input only integer numbers between 1 and 10!')
     except ValueError:
-      print("Please, input only integer numbers between 1 and 10!")
-      continue
+        print('Please, input only integer numbers between 1 and 10!')
 
 while number != user_number:
-    print ("Please, select another number!")
-    user_number = int(input())
+    try:
+        user_number = int(input('Please, select another number!'))
+        if not 10 >= user_number >= 1:
+            print('Please, input only integer numbers between 1 and 10!')
+            continue
+    except ValueError:
+        print('Please, input only integer numbers between 1 and 10!')
+        continue
     if user_number > number:
-      print("Too high!")
+        print('Too high!')
     elif user_number < number:
-      print("Too low!")
+        print('Too low!')
     else:
-      print("Awesome! The number is correct!")
-      break
+        print('Awesome! The number is correct!')
+        break
